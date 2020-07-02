834010f092f8cee50ef0395d5ce18d4659f5d096
Rohit1220 <65952260+Rohit1220@users.noreply.github.com>
2020-07-02 11:48:14 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,32 +1,41 @@
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
-var engine, world;
-var bird, slingshot;
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
-    log5 = new Log(870,120,150, -PI/7);
-    bird = new Bird(200,50);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
+const Body = Matter.Body;
+//const Render = Matter.Render;
+var groundObject	
+var world;
+function setup() {
+	createCanvas(1600,700);
+	rectMode(CENTER);
+	engine = Engine.create();
+	world = engine.world;
+	bob1=new bob(200,450,40);
+	bob2=new bob(250,450,40);
+	bob3=new bob(300,450,40);
+	bob4=new bob(350,450,40);
+	groundObject=new ground(width/2,100,400,20);
+	sling1 = new SlingShot(bob1.body,groundObject.body);
+	sling2 = new SlingShot(bob2.body,groundObject.body);
+	sling3 = new SlingShot(bob3.body,groundObject.body);
+	sling4 = new SlingShot(bob4.body,groundObject.body);
+	Engine.run(engine);
 }
-function draw(){
-    background("white")
-    Engine.update(engine);
-    log5.display();
-    bird.display();
-    slingshot.display();
-}
-function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-}
-function mouseReleased(){
-    slingshot.fly();
-}
-function keyPressed(){
-    if(keyCode === 32){
-        slingshot.attach(bird.body);
-    }
+function draw() {
+  rectMode(CENTER);
+  background(225);
+  if(keyCode === 32)
+  {
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  groundObject.display();
+  sling1.display();
+  sling2.display();
+  sling3.display();
+  sling4.display();
+  }
+  drawSprites();
 }
---
+++ slingshot.js
@@ -0,0 +1,25 @@
+class SlingShot{
+    constructor(bodyA, bodyB){
+        var options = {
+            bodyA: bodyA,
+            bodyB:bodyB,
+            stiffness: 1,
+            length: 300
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.sling.bodyB.position;
+            push();
+            stroke(48,22,8);
+            if(pointA.x < 220) {
+                strokeWeight(7);
+                line(pointA.x , pointA.y, pointB.x , pointB.y);
+            }
+            pop();
+        }
+    }   
+}
